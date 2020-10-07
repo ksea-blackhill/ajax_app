afe288cef74620d4afc044de9bfbf5dc86107e72
ksea-blackhill <71749222+ksea-blackhill@users.noreply.github.com>
2020-10-07 12:07:13 +0900

    AjaxAppの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
+  root to: 'posts#index'
+  #get 'posts/new', to: 'posts#new' ←削除機能
   post 'posts', to: 'posts#create'
 end
